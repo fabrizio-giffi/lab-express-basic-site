3f7512389a2b319b2fb6ecd756f9800c68d85221
Fabrizio Giffi <fabrizio.giffi@gmail.com>
2023-01-31 15:19:28 +0100

    Minor changes to HTML files. Structure upgraded with EJS.

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
 const express = require('express')
 const path = require('path')
+const expressLayouts = require('express-ejs-layouts');
 const app = express()
 const port = 3000
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { title: "Homepage" });
 })
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { title: "About" });
 })
 
 app.get('/discography', (req, res) => {
-    res.render('discography');
+    res.render('discography', { title: "Discography" });
 })
 
 app.get('/photos', (req, res) => {
-    res.render('photos');
+    res.render('photos', { title: "Photo gallery" });
   })
 
 app.listen(port, () => {
